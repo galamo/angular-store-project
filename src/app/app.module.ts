29ea466000c6140c08e8a1bf45b0f73fcdd8ec30
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2021-03-10 19:54:48 +0200

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms"
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './components/product/product.component';
+import { MatCardModule } from "@angular/material/card"
+import { MatButtonModule } from "@angular/material/button"
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ProductComponent,
   ], // components / directives/ pipes and all module related
   imports: [
-    BrowserModule, FormsModule // other modules we want to use
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule// other modules we want to use
   ],
   providers: [], // services 
   bootstrap: [AppComponent] //root component
