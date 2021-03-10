0e61829d423de8af9c54a5586af4efff914f5a7d
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2021-03-10 18:27:51 +0200

    ng

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core"
 
+
+interface IColorTheme {
+    label: string
+    value: string
+}
+
 @Component({
     selector: 'header-app', //html tag - use with <header-app></header-app>
     templateUrl: "./header.component.html",
@@ -7,12 +13,27 @@ import { Component } from "@angular/core"
 })
 export class HeaderComponent {
     public title: string
+    public center: string
+    public color: string = "blue"
     public products = ["Milk", "Eggs", "Bread", "Coke", "Water"];
+    public showElement: boolean = false;
+    public colors: Array<IColorTheme> = [{ label: "unicorn", value: "pink" },
+    { label: "purple&indigo", value: "purple" },
+    { label: "default", value: "blue" }]
     constructor() {
         this.title = "Store Application"
+        this.center = ""
     }
 
     changeHeader(newTitle: string) {
         this.title = newTitle
+        this.center = "width:300px;margin:auto auto"
+    }
+
+    changeColor(color: string) {
+        this.color = color
+    }
+    showPanel() {
+        this.showElement = !this.showElement;
     }
 }
